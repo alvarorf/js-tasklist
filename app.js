8cd074639e2144a18a8cc6917e291f29bec7d4f3
alvaro <aaruizf@unal.edu.co>
2020-07-28 18:10:33 -0400

    Added some materialize styling and completed the function addTask in app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners(){
+  // Add task event
+  form.addEventListener('submit', addTask);
+
+}
+
+// Add task
+function addTask(e){
+  if(taskInput.value === ''){
+    alert('Add a task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>'
+  // Append the link to li
+  li.appendChild(link);
+  // Now, we append li to ul (we have the ul selected with the .collection class,
+  // and stored in the variable taskList)
+  taskList.appendChild(li);
+  // Finally, we clear the input
+  taskInput.value="";
+
+  console.log(li);
+  e.preventDefault();
+}
