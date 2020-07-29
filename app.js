58aedd713a7bdbeafcbb30dcf200c5134d6fdce3
alvaro <aaruizf@unal.edu.co>
2020-07-28 20:13:54 -0400

    Added clearTasks and filterTasks functions

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners(){
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click',clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup',filterTasks);
 
 }
 
@@ -41,6 +47,47 @@ function addTask(e){
   // Finally, we clear the input
   taskInput.value="";
 
-  console.log(li);
+  //console.log(li);
   e.preventDefault();
+}
+
+
+// Remove task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains
+    ('delete-item')) 
+  {
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+    
+  }
+  
+}
+
+// Clear task
+function clearTasks() {
+  // taskList.innerHTML = '';
+  // FASTER: We iterate through each element
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach();
+  (function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text)!=-1){
+      task.style.display = 'block';
+
+    } else {
+      task.style.display = 'none';
+
+    }
+
+  });
 }
